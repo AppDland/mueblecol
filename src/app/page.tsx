5c420946f72b9cded9c494731d20901a43931f4c
Juan David <juancho.1998.0611@outlook.com>
2024-11-19 18:36:51 -0300

    Create Page

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Card, { ItemInt } from "@/components/Card";
+import Finder from "@/components/Finder";
+import Items from "@/data/items.json";
+import { useState } from "react";
+
+export default function Home() {
+    const [text, setText] = useState('');
+    const [found, setFound] = useState<ItemInt[]>([]);
+
+    const handleFind = () => {
+        console.log(Items.filter(({ name }) => name.includes(text)))
+        setFound(Items.filter(({ name }) => name.toLowerCase().includes(text.toLowerCase())));
+    }
+
+    return (
+        <div className="flex flex-wrap">
+            <Finder value={text} setValue={setText} onFind={handleFind} />
+            {
+                found.map((item, index) => (
+                    <Card item={item} key={index} />
+                ))
+            }
+        </div>
+    );
+}
---
+++ src/components/Finder.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { useState } from "react";
+
+interface FinderInt {
+    value: string;
+    setValue: (param: string) => void;
+    onFind: () => void;
+}
+
+const Finder = ({ value, setValue, onFind }: FinderInt) => {
+
+    const [isFocused, setIsfocused] = useState(false);
+
+    const handleBlur = () => {
+        if (value.length === 0) {
+            setIsfocused(false)
+        }
+    }
+
+    const handleKeyUp = (event: any) => {
+        if (event.code === "Enter" || event.key === "Enter" || event.keyCode === 13) {
+            onFind();
+        }
+    }
+
+    return (
+        <div className="relative w-80 h-12 m-5 overflow-hidden rounded-lg flex items-center justify-center border border-[#A30000]">
+            <input
+                className={`
+                relative 
+                bg-transparent 
+                z-10 
+                w-full 
+                h-full 
+                px-4 
+                placeholder:text-center 
+                text-xl
+                placeholder:text-[#E8E8E8] 
+                box-content
+                outline-none
+                ${isFocused ? 'cursor-auto' : 'cursor-pointer'}
+                `}
+                type="text"
+                placeholder="¿Qué deseas buscar?"
+                onChange={({ target }) => setValue(target.value)}
+                onFocus={() => setIsfocused(true)}
+                onBlur={handleBlur}
+                onKeyUp={handleKeyUp}
+            />
+            <img
+                src="/Lupa.svg"
+                className="relative z-10 w-7 mx-2 cursor-pointer"
+                onClick={onFind}
+            />
+            <div className={`
+                bg-[#A30000]
+                ${isFocused ? "w-11" : "w-full"}
+                h-full
+                absolute
+                right-0
+                top-0
+                z-0
+                duration-100
+            `}
+            />
+        </div>
+    )
+}
+
+export default Finder;
